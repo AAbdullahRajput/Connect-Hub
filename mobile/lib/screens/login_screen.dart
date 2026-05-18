42a1d7cc671a9e8c30aa65c6a512d9d210987f71
Ahmad Abdullah <abdullahhrajput4943@gmail.com>
2026-05-19 04:12:10 +0500

    Updated Paths

--- mobile/lib/screens/login_screen.dart
+++ mobile/lib/screens/login_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/auth_provider.dart';
 
+
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
